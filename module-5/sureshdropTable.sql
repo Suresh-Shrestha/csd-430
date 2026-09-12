54e4b6fdab0058c06fa3a6d41204b69455d49117
Suresh <sureshshrestha836@gmail.com>
2026-09-11 22:29:48 -0500

    Modules 5.2 and 6.2 database setup

---
+++ module-5/sureshdropTable.sql
@@ -0,0 +1,10 @@
+/*
+    Name: Suresh Shrestha
+    Module: 5
+    Date: 9/11/2026
+    Description: Drops the movies data table from the CSD430 database.
+*/
+
+USE CSD430;
+
+DROP TABLE IF EXISTS sureshmoviesdata;
